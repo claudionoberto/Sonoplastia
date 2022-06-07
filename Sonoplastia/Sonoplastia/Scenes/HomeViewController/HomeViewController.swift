43d98e66a733e064fcd060273a1179f4f1b718cd
Claudio Noberto <claudionoberto9@gmail.com>
2022-06-07 13:10:53 -0400

    Projeto Formatado

---
+++ Sonoplastia/Sonoplastia/Scenes/HomeViewController/HomeViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  Sonoplastia
+//
+//  Created by Claudio Noberto on 07/06/22.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+}
---
+++ Sonoplastia/Sonoplastia/Sources/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  Sonoplastia
+//
+//  Created by Claudio Noberto on 07/06/22.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        let viewController = HomeViewController()
+        let navController = UINavigationController(rootViewController: viewController)
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = navController
+        window?.makeKeyAndVisible()
+        return true
+    }
+}
