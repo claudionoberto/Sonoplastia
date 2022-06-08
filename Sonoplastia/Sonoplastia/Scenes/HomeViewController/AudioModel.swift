b538befc9531c9549abf7f4a6c61fe488146be12
francyanne <francyannemartins1@gmail.com>
2022-06-08 15:28:00 -0400

    Adicionei audioModel, alterei os nomes das celulas e adicionei os sons

---
+++ Sonoplastia/Sonoplastia/Scenes/HomeViewController/AudioModel.swift
@@ -0,0 +1,13 @@
+//
+//  AudioModel.swift
+//  Sonoplastia
+//
+//  Created by Fran Martins on 08/06/22.
+//
+
+import Foundation
+
+struct AudioModel {
+    let name: String
+    let assetName: String
+}
--- Sonoplastia/Sonoplastia/Scenes/HomeViewController/HomeViewController.swift
+++ Sonoplastia/Sonoplastia/Scenes/HomeViewController/HomeViewController.swift
@@ -11,15 +11,48 @@ import AVFoundation
 class HomeViewController: UIViewController {
     lazy var tableView = UITableView(frame: .zero, style: .insetGrouped)
     lazy var player = AVAudioPlayer()
+    var audios = [AudioModel]()
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Sonoplastia"
         configTableView()
+        configData()
     }
         
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         tableView.frame = view.bounds
+    }
+    
+    func configData() {
+        let audioOne = AudioModel(name: "Ratinho", assetName: "Ratinhoo")
+        let audioTwo = AudioModel(name: "Ai", assetName: "Ai")
+        let audioThree = AudioModel(name: "Cavalo", assetName: "Cavalo")
+        let audioFour = AudioModel(name: "Dança Gatinho", assetName: "DancaGatinhoDanca")
+        let audioFive = AudioModel(name: "Demais", assetName: "Demais")
+        let audioSix = AudioModel(name: "Ele Gosta", assetName: "EleGosta")
+        let audioSeven = AudioModel(name: "Não", assetName: "Nao")
+        let audioEight = AudioModel(name: "Pare", assetName: "Pare")
+        let audioNine = AudioModel(name: "Rapaz", assetName: "Rapais")
+        let audioTen = AudioModel(name: "Atumalaca", assetName: "Risada")
+        let audioEleven = AudioModel(name: "Tome", assetName: "Tome")
+        let audioTwelve = AudioModel(name: "Tapa", assetName: "Tuff")
+        let audioThirteen = AudioModel(name: "Ui", assetName: "Ui")
+        let audioFourteen = AudioModel(name: "Vamo Dança", assetName: "VamoDancar")
+        audios.append(audioOne)
+        audios.append(audioTwo)
+        audios.append(audioThree)
+        audios.append(audioFour)
+        audios.append(audioFive)
+        audios.append(audioSix)
+        audios.append(audioSeven)
+        audios.append(audioEight)
+        audios.append(audioNine)
+        audios.append(audioTen)
+        audios.append(audioEleven)
+        audios.append(audioTwelve)
+        audios.append(audioThirteen)
+        audios.append(audioFourteen)
     }
     
     func configTableView() {
@@ -46,16 +79,18 @@ extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
         return "Sons"
     }
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return audios.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-        cell.textLabel?.text = "Test"
+        let data = audios[indexPath.row]
+        cell.textLabel?.text = data.name
         return cell
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
+        playAudio(audioName: audios[indexPath.row].assetName)
     }
 }
