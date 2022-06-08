dd885b22d09d4aee63e02aa519ee815830aba694
Claudio Noberto <claudionoberto9@gmail.com>
2022-06-08 11:11:11 -0400

    Adicionando configuração da tableview

--- Sonoplastia/Sonoplastia/Scenes/HomeViewController/HomeViewController.swift
+++ Sonoplastia/Sonoplastia/Scenes/HomeViewController/HomeViewController.swift
@@ -8,9 +8,38 @@
 import UIKit
 
 class HomeViewController: UIViewController {
-
+    lazy var tableView = UITableView(frame: .zero, style: .insetGrouped)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .white
+        title = "Sonoplastia"
+        configTableView()
+    }
+        
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        tableView.frame = view.bounds
+    }
+    
+    func configTableView() {
+        view.addSubview(tableView)
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+}
+
+extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return "Sons"
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = "Test"
+        return cell
     }
 }
