674359b3acfcdb5362d182ff83198ce1d7b12590
Claudio Noberto <claudionoberto9@gmail.com>
2022-06-12 16:08:58 -0400

    Refatorando funções e adicionando imageView nas células

--- Sonoplastia/Sonoplastia/Scenes/HomeViewController/HomeViewController.swift
+++ Sonoplastia/Sonoplastia/Scenes/HomeViewController/HomeViewController.swift
@@ -9,44 +9,40 @@ import UIKit
 import AVFoundation
 
 class HomeViewController: UIViewController {
+    
     lazy var tableView = UITableView(frame: .zero, style: .insetGrouped)
     lazy var player = AVAudioPlayer()
-    var selectedAudios: [String] = []
-    var audios = [AudioModel]()
+    lazy var selectedAudios: [String] = []
+    var audios: [AudioModel] = [AudioModel(name: "Ratinho", assetName: "Ratinho_audio"),
+                                AudioModel(name: "Ai", assetName: "Ai_audio"),
+                                AudioModel(name: "Cavalo", assetName: "Cavalo_audio"),
+                                AudioModel(name: "Dança Gatinho", assetName: "DancaGatinho_audio"),
+                                AudioModel(name: "Demais", assetName: "Demais_audio"),
+                                AudioModel(name: "Ele Gosta", assetName: "EleGosta_audio"),
+                                AudioModel(name: "Não", assetName: "Nao_audio"),
+                                AudioModel(name: "Pare", assetName: "Pare_audio"),
+                                AudioModel(name: "Rapaz", assetName: "Rapaz_audio"),
+                                AudioModel(name: "Atumalaca", assetName: "Atumalaca_audio"),
+                                AudioModel(name: "Tome", assetName: "Tome_audio"),
+                                AudioModel(name: "Tapa", assetName: "Tapa_audio"),
+                                AudioModel(name: "Ui", assetName: "Ui_audio"),
+                                AudioModel(name: "Vamo Dança", assetName: "VamoDancar_audio")
+    ]
  
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Sonoplastia"
         navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Cancelar", style: .plain, target: self, action: #selector(cancelButtonAction))
         navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(shareButtonAction))
-        configTableViewEditing(isEditing: false, isHidden: true)
+        self.configTableViewEditing(editingMode: false)
         configTableView()
-        configData()
     }
         
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         tableView.frame = view.bounds
     }
-    
-    func configData() {
-        audios = [AudioModel(name: "Ratinho", assetName: "Ratinho_audio"),
-                  AudioModel(name: "Ai", assetName: "Ai_audio"),
-                  AudioModel(name: "Cavalo", assetName: "Cavalo_audio"),
-                  AudioModel(name: "Dança Gatinho", assetName: "DancaGatinho_audio"),
-                  AudioModel(name: "Demais", assetName: "Demais_audio"),
-                  AudioModel(name: "Ele Gosta", assetName: "EleGosta_audio"),
-                  AudioModel(name: "Não", assetName: "Nao_audio"),
-                  AudioModel(name: "Pare", assetName: "Pare_audio"),
-                  AudioModel(name: "Rapaz", assetName: "Rapaz_audio"),
-                  AudioModel(name: "Atumalaca", assetName: "Atumalaca_audio"),
-                  AudioModel(name: "Tome", assetName: "Tome_audio"),
-                  AudioModel(name: "Tapa", assetName: "Tapa_audio"),
-                  AudioModel(name: "Ui", assetName: "Ui_audio"),
-                  AudioModel(name: "Vamo Dança", assetName: "VamoDancar_audio")
-        ]
-    }
-    
+ 
     func configTableView() {
         view.addSubview(tableView)
         tableView.allowsMultipleSelectionDuringEditing = true
@@ -67,7 +63,7 @@ class HomeViewController: UIViewController {
     }
     
     @objc func cancelButtonAction() {
-        configTableViewEditing(isEditing: false, isHidden: true)
+        configTableViewEditing(editingMode: false)
         selectedAudios.removeAll()
     }
     
@@ -78,23 +74,27 @@ class HomeViewController: UIViewController {
             activityItems.append(activityItem)
         }
         
-        let activityVC = UIActivityViewController(activityItems: activityItems,applicationActivities: nil)
+        let activityVC = UIActivityViewController(activityItems: activityItems, applicationActivities: nil)
         activityVC.completionWithItemsHandler = { [weak self] _, success, _, _ in
             guard let self = self else { return }
             
             if success {
-                self.configTableViewEditing(isEditing: false, isHidden: true)
+                self.configTableViewEditing(editingMode: false)
                 self.selectedAudios.removeAll()
             }
         }
-        activityVC.popoverPresentationController?.sourceView = self.view
-        self.present(activityVC, animated: true)
+        present(activityVC, animated: true)
     }
     
-    func configTableViewEditing(isEditing: Bool, isHidden: Bool) {
-        tableView.setEditing(isEditing, animated: true)
-        navigationItem.rightBarButtonItem?.isHidden = isHidden
-        navigationItem.leftBarButtonItem?.isHidden = isHidden
+    func configTableViewEditing(editingMode: Bool) {
+        tableView.setEditing(editingMode, animated: true)
+        if editingMode {
+            navigationItem.rightBarButtonItem?.isHidden = false
+            navigationItem.leftBarButtonItem?.isHidden = false
+        } else {
+            navigationItem.rightBarButtonItem?.isHidden = true
+            navigationItem.leftBarButtonItem?.isHidden = true
+        }
     }
 }
 
@@ -110,6 +110,7 @@ extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
         let data = audios[indexPath.row]
         cell.textLabel?.text = data.name
+        cell.imageView?.image = UIImage(systemName: "music.note")
         return cell
     }
     func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
@@ -133,7 +134,7 @@ extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
     func makeContextMenu(indexPath: IndexPath) -> UIMenu {
         let share = UIAction(title: "Compartilhar", image: UIImage(systemName: "square.and.arrow.up")) { [weak self] action in
             guard let self = self else { return }
-            self.configTableViewEditing(isEditing: true, isHidden: false)
+            self.configTableViewEditing(editingMode: true)
             self.tableView.selectRow(at: indexPath, animated: true, scrollPosition: .none)
             self.selectedAudios.append(self.audios[indexPath.row].assetName)
         }
